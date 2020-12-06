dd6b2685a97ea9b9953a436d2f1c26b4fa504a37
Nguyen Tran <NguyenTC@F99.com.vn>
2020-12-06 21:35:24 +0700

    Study | FlatList | Add lib react-native-swipeout and swipe to delete item in flatList

--- Tutorial/components/17_FlatList_01.js
+++ Tutorial/components/17_FlatList_01.js
@@ -1,80 +1,146 @@
 import React, {Component} from 'react';
 import {
-    View,
-    Image,
-    Text,
-    ScrollView,
-    TextInput,
-    Dimensions,
-    StyleSheet,
-    FlatList
+  View,
+  Image,
+  Text,
+  ScrollView,
+  TextInput,
+  Dimensions,
+  StyleSheet,
+  FlatList,
+  Alert,
 } from 'react-native';
-import flastListData from './data/flatListData';
+import flatListData from './data/flatListData';
+import Swipeout from 'react-native-swipeout';
 
 export default class BasicFlatList extends Component {
-    render() {
-        return(
-            <View
-                style={{flex: 1, marginTop: 33}}
-            >
-                <FlatList
-                    data={flastListData}
-                    renderItem={({item, index}) => {
-                        return(
-                            <FlastListItem
-                                item={item} index={index}    
-                            ></FlastListItem>
-                        );
-                    }}
-                >
+  constructor(props) {
+    super(props);
+    this.state = {
+      deleteKeyRow: null,
+    };
+  }
 
-                </FlatList>
-            </View>
-        );
-    }
+  refreshFlatList = (deleteKey) => {
+    this.setState((prevState) => {
+      return {deleteKeyRow: deleteKey};
+    });
+  };
+
+  render() {
+    return (
+      <View style={{flex: 1, marginTop: 33}}>
+        <FlatList
+          data={flatListData}
+          renderItem={({item, index}) => {
+            return (
+              <FlatListItem item={item} index={index} parentFlatList={this} />
+            );
+          }}
+        />
+      </View>
+    );
+  }
 }
 
-class FlastListItem extends Component {
-    render() {
-        return(
-            <View>
-                <View
-                style={{
-                    flex: 1,
-                    flexDirection: 'row',
-                    // backgroundColor: this.props.index % 2 == 0 ? 'mediumseagreen' : 'tomato'
-                    backgroundColor: "green" 
-                }}
-            >
-                <Image
-                    source={{uri: this.props.imageUrl}}
-                    style={{height: 100, width: 100, backgroundColor: 'white', margin: 10}}
-                ></Image>
-                <View
-                    style={{
-                        flex: 1,
-                        flexDirection: 'column'
-                    }}
-                >
-                <Text style={styles.itemText, {fontSize: 20}}>{this.props.item.name}</Text>
-                <Text style={styles.itemText}>{this.props.item.foodDescription}</Text>
-                </View>
-            </View>
+class FlatListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeRowKey: null,
+    };
+  }
+  render() {
+    const swipeSettings = {
+      autoClose: true,
+      onClose: (secId, rowId, direction) => {
+        if (this.state.activeRowKey != null) {
+          this.setState({activeRowKey: null});
+        }
+      },
+      onOpen: (secId, rowId, direction) => {
+        this.setState({activeRowKey: this.props.item.key});
+      },
+      right: [
+        {
+          onPress: () => {
+            const deleteRow = this.props.activeRowKey;
+            Alert.alert(
+              'Alert',
+              'Are you want to delete row ?',
+              [
+                {
+                  text: 'No',
+                  onPress: () => console.log('Cancel Pressed'),
+                  style: 'cancel',
+                },
+                {
+                  text: 'Yes',
+                  onPress: () => {
+                    flatListData.splice(this.props.index, 1);
+                    // refresh FlatList
+                    this.props.parentFlatList.refreshFlatList(deleteRow);
+                  },
+                },
+              ],
+              {cancelable: true},
+            );
+          },
+          text: 'Delete',
+          type: 'delete',
+        },
+      ],
+      rowId: this.props.index,
+      sectionId: 1,
+    };
+    return (
+      <Swipeout {...swipeSettings}>
+        <View>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              // backgroundColor: this.props.index % 2 == 0 ? 'mediumseagreen' : 'tomato'
+              backgroundColor: 'green',
+            }}>
+            <Image
+              source={{uri: this.props.imageUrl}}
+              style={{
+                height: 100,
+                width: 100,
+                backgroundColor: 'white',
+                margin: 10,
+              }}
+            />
             <View
-                style={{
-                    backgroundColor: "white",
-                    height: 1,
-                }}
-            ></View>
+              style={{
+                flex: 1,
+                flexDirection: 'column',
+              }}>
+              <Text style={(styles.itemText, {fontSize: 20})}>
+                {this.props.item.name}
+              </Text>
+              <Text style={styles.itemText}>
+                {this.props.item.foodDescription}
+              </Text>
             </View>
-        );
-    }
+          </View>
+          <View
+            style={{
+              backgroundColor: 'white',
+              height: 1,
+            }}
+          />
+        </View>
+      </Swipeout>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    itemText: {
-        color: "white",
-        padding: 10,
-        fontSize: 15,
-    }
+  itemText: {
+    color: 'white',
+    padding: 10,
+    fontSize: 15,
+  },
 });
