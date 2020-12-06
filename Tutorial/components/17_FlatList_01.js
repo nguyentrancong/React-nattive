7606acd7aee23b1b5341901e52ac889bffeddd8d
Nguyen Tran <NguyenTC@F99.com.vn>
2020-12-06 20:52:38 +0700

    Study | FlatList | add image in item

--- Tutorial/components/17_FlatList_01.js
+++ Tutorial/components/17_FlatList_01.js
@@ -37,14 +37,35 @@ export default class BasicFlatList extends Component {
 class FlastListItem extends Component {
     render() {
         return(
-            <View
+            <View>
+                <View
                 style={{
                     flex: 1,
-                    backgroundColor: this.props.index % 2 == 0 ? 'mediumseagreen' : 'tomato' 
+                    flexDirection: 'row',
+                    // backgroundColor: this.props.index % 2 == 0 ? 'mediumseagreen' : 'tomato'
+                    backgroundColor: "green" 
                 }}
             >
+                <Image
+                    source={{uri: this.props.imageUrl}}
+                    style={{height: 100, width: 100, backgroundColor: 'white', margin: 10}}
+                ></Image>
+                <View
+                    style={{
+                        flex: 1,
+                        flexDirection: 'column'
+                    }}
+                >
                 <Text style={styles.itemText, {fontSize: 20}}>{this.props.item.name}</Text>
                 <Text style={styles.itemText}>{this.props.item.foodDescription}</Text>
+                </View>
+            </View>
+            <View
+                style={{
+                    backgroundColor: "white",
+                    height: 1,
+                }}
+            ></View>
             </View>
         );
     }
--- Tutorial/components/data/flatListData.js
+++ Tutorial/components/data/flatListData.js
@@ -2,76 +2,91 @@ var flatListData = [
     {
         "key": "foodDescription",
         "name": "Autoplay Policy Changes",
+        "imageUrl": "https://reactnative.dev/docs/assets/p_cat2.png",
         "foodDescription": "Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
     {
         "key": "1foodDescription",
         "name": "1Autoplay Policy Changes",
+        "imageUrl": "https://user-images.githubusercontent.com/29597/63811956-bdd9b580-c8dd-11e9-8962-c568e475c425.png",
         "foodDescription": "1Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
     {
         "key": "2foodDescription",
         "name": "2Autoplay Policy Changes",
+        "imageUrl": "//i.imgflip.com/ngd6c.jpg",
         "foodDescription": "2Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
     {
         "key": "3foodDescription",
         "name": "3Autoplay Policy Changes",
+        "imageUrl": "https://user-images.githubusercontent.com/29597/63811956-bdd9b580-c8dd-11e9-8962-c568e475c425.png",
         "foodDescription": "3Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
     {
         "key": "4foodDescription",
         "name": "4Autoplay Policy Changes",
+        "imageUrl": "https://user-images.githubusercontent.com/29597/63811956-bdd9b580-c8dd-11e9-8962-c568e475c425.png",
         "foodDescription": "4Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
     {
         "key": "5foodDescription",
         "name": "5Autoplay Policy Changes",
+        "imageUrl": "https://user-images.githubusercontent.com/29597/63811956-bdd9b580-c8dd-11e9-8962-c568e475c425.png",
         "foodDescription": "5Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
     {
         "key": "6foodDescription",
         "name": "6Autoplay Policy Changes",
+        "imageUrl": "https://user-images.githubusercontent.com/29597/63811956-bdd9b580-c8dd-11e9-8962-c568e475c425.png",
         "foodDescription": "6Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
     {
         "key": "7foodDescription",
         "name": "7A7utoplay Policy Changes",
+        "imageUrl": "https://user-images.githubusercontent.com/29597/63811956-bdd9b580-c8dd-11e9-8962-c568e475c425.png",
         "foodDescription": "7Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
     {
         "key": "8foodDescription",
         "name": "8Autoplay Policy Changes",
+        "imageUrl": "https://user-images.githubusercontent.com/29597/63811956-bdd9b580-c8dd-11e9-8962-c568e475c425.png",
         "foodDescription": "8Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
     {
         "key": "9foodDescription",
         "name": "9Autoplay Policy Changes",
+        "imageUrl": "https://user-images.githubusercontent.com/29597/63811956-bdd9b580-c8dd-11e9-8962-c568e475c425.png",
         "foodDescription": "9Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
     {
         "key": "f12oodDescription",
         "name": "222Autoplay Policy Changes",
+        "imageUrl": "https://user-images.githubusercontent.com/29597/63811956-bdd9b580-c8dd-11e9-8962-c568e475c425.png",
         "foodDescription": "Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
     {
         "key": "22foodDescription",
         "name": "22Autoplay Policy Changes",
+        "imageUrl": "https://user-images.githubusercontent.com/29597/63811956-bdd9b580-c8dd-11e9-8962-c568e475c425.png",
         "foodDescription": "Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
     {
         "key": "333foodDescription",
         "name": "333Autoplay Policy Changes",
+        "imageUrl": "https://user-images.githubusercontent.com/29597/63811956-bdd9b580-c8dd-11e9-8962-c568e475c425.png",
         "foodDescription": "Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
     {
         "key": "4444foodDescription",
         "name": "4444Autoplay Policy Changes",
+        "imageUrl": "https://user-images.githubusercontent.com/29597/63811956-bdd9b580-c8dd-11e9-8962-c568e475c425.png",
         "foodDescription": "Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
     {
         "key": "555555foodDescription",
         "name": "5555Autoplay Policy Changes",
+        "imageUrl": "https://user-images.githubusercontent.com/29597/63811956-bdd9b580-c8dd-11e9-8962-c568e475c425.png",
         "foodDescription": "Chrome's autoplay policies will change in April of 2018 and I'm here to tell you why and how this is going to affect video playback with sound. Spoiler alert: users are going to love it!"
     },
 ]
